97aef4a417be4067f6b128a85e0bae3fcfcafaa3
Tim Urista <timothy.urista@gmail.com>
2019-01-13 20:10:47 -0800

    add solution for map to country with patents

--- graph/country-map/country-patents.js
+++ graph/country-map/country-patents.js
@@ -1,8 +1,8 @@
 console.log("DATA", topCountries, countryCodeMap);
 var data = topCountries;
 
-let width = 800;
-let height = 600;
+var width = 960;
+var height = 500;
 var svg = d3
   .select("#graph")
   .append("svg")
@@ -13,7 +13,7 @@ let center = [width / 2, height / 2];
 var projection = d3
   .geoMercator()
   .scale(width / 2 / Math.PI)
-  .translate([width / 2, height / 2]);
+  .translate([width / 2, 50 + height / 2]);
 
 var path = d3.geoPath().projection(projection);
 
@@ -30,21 +30,123 @@ var geoPoint = {
 
 var g = svg.append("g");
 
-d3.json("world-110m.json", function(error, topology) {
-  g.selectAll("path")
-    .data(topojson.object(topology, topology.objects.countries).geometries)
-    .enter()
-    .append("path")
-    .attr("d", path);
-});
+// fetch("world-110m.v1.json")
+//   .then(res => res.json())
+//   .then(topology => {
+console.log(worldTopo);
+let topology = worldTopo;
+var map = g
+  .selectAll("path")
+  .data(topojson.object(topology, topology.objects.countries).geometries)
+  .enter()
+  .append("path")
+  .attr("d", d => {
+    console.log("MAP LOADING", path(d), d);
+    return path(d);
+  });
+
+// });
 
 function update() {
-  console.log("updating...", path(geoPoint));
-  // g.append("circle")
-  //   .attr("cx", 30)
-  //   .attr("cy", 30)
-  //   .attr("r", 20);
-  g.append("path").attr("d", path(geoPolygon));
+  aa = [-122.490402, 37.786453];
+  bb = [-122.389809, 37.72728];
+
+  console.log("updating...");
+
+  g.append("g")
+    .selectAll("circle")
+    .data(countryCords)
+    .enter()
+    .append("g")
+    .attr("class", "count-circle")
+    .append("circle")
+    .attr("cx", function(d) {
+      console.log("CX", d);
+      return projection(d)[0];
+    })
+    .attr("cy", function(d) {
+      console.log("CY", d, projection(d));
+      return projection(d)[1];
+    })
+    .attr("r", function(d, i) {
+      if (topCountries[i]) {
+        const weight =
+          topCountries[i].patentCount / topCountries[0].patentCount;
+        return `${Math.floor(weight * 30 + 15).toFixed(0)}px`;
+      }
+    })
+    .attr("fill", "red");
+
+  // console.log("CIRLCES", d3.selectAll(".count-circle"));
+  // d3.selectAll(".count-circle")
+  //   .append("text")
+  //   .attr("x", "50%")
+  //   .attr("y", "50%")
+  //   .attr("text-anchor", "middle")
+  //   .attr("dy", "5em")
+  //   .attr("fill", "white");
+
+  // .attr("x", function(d) {
+  //   console.log("X", d);
+  //   return projection(d)[0];
+  // })
+  // .attr("y", function(d) {
+  //   console.log("Y", d, projection(d));
+  //   return projection(d)[1];
+  // })
+  // .text(function(d, i) {
+  //   if (topCountries[i]) {
+  //     return topCountries[i].patentCount;
+  //   }
+  // })
+  // .attr("fill", "white");
+
+  g.append("g")
+    .selectAll("text")
+    .data(countryCords)
+    .enter()
+    .append("text")
+    .attr("x", function(d) {
+      console.log("X", d);
+      return projection(d)[0];
+    })
+    .attr("y", function(d) {
+      console.log("Y", d, projection(d));
+      return projection(d)[1];
+    })
+    .text(function(d, i) {
+      if (topCountries[i]) {
+        return topCountries[i].patentCount;
+      }
+    })
+    .attr("dy", ".3em")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white");
+
+  // .selectAll("path")
+  // .data(countryCords)
+  // .enter()
+  // .append("path")
+  // .attr("d", d => {
+  //   console.log("ENTERING", path(d), d);
+  //   return d3.geoPath().projection(d);
+  // })
+  // .attr("fill", "red");
+
+  // g.data(data)
+  //   .append("circle")
+  //   .attr("cx", d => {
+  //     const geo = [Number(d.lat), Number(d.long)];
+  //     console.log(d);
+  //     return projection(geo)[0];
+  //   })
+  //   .attr("cy", d => {
+  //     const geo = [Number(d.lat), Number(d.long)];
+  //     console.log(projection(geo));
+  //     return projection(geo)[1];
+  //   })
+  //   .attr("r", "20px")
+  //   .attr("fill", "red");
 }
 
 update();
